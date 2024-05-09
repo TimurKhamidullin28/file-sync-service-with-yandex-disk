fd7dfcabe68465f356dd32687c66de1d66d862e4
Timur Khamidullin <timfoss2801@gmail.com>
2024-05-10 04:51:15 +0500

    try-except block added

--- main.py
+++ main.py
@@ -12,40 +12,42 @@ def sync_files(connector: ConnectorWithCloudService, logger_file: logger) -> Non
     :param connector: работающий с облачным сервисом объект класса
     :param logger_file: логгер из модуля loguru
     """
-    remote_files = connector.get_info_remote()
-    local_files = connector.get_info_local()
-
-    if remote_files:
-        for i_file in remote_files.keys():
-            # Условие, при котором файл удаляется из облачного хранилища:
-            if i_file not in local_files.keys():
-                try:
-                    connector.delete_file(i_file)
-                    logger_file.info('Файл {} успешно удален'.format(i_file))
-
-                except (Timeout, TooManyRedirects, ConnectionError):
-                    logger_file.error('Файл {} не удален. Ошибка соединения.'.format(i_file))
-
-    for i_file in local_files.keys():
-        # Условие, при котором файл записывается в облачное хранилище:
-        if not remote_files or i_file not in remote_files.keys():
-            try:
-                connector.load_file(i_file)
-                logger_file.info('Файл {} успешно записан'.format(i_file))
-            except (Timeout, TooManyRedirects, ConnectionError):
-                logger_file.error('Файл {} не записан. Ошибка соединения.'.format(i_file))
-            except OSError:
-                logger_file.error("Ошибка чтения файла {}".format(i_file))
+    try:
+        local_files = connector.get_info_local()
+        remote_files = connector.get_info_remote()
         if remote_files:
-            # Условие, при котором файл обновляется в облачном хранилище:
-            if i_file in remote_files.keys() and local_files[i_file] > remote_files[i_file]:
+            for i_file in remote_files.keys():
+                # Условие, при котором файл удаляется из облачного хранилища:
+                if i_file not in local_files.keys():
+                    try:
+                        connector.delete_file(i_file)
+                        logger_file.info('Файл {} успешно удален'.format(i_file))
+
+                    except (Timeout, TooManyRedirects, ConnectionError):
+                        logger_file.error('Файл {} не удален. Ошибка соединения.'.format(i_file))
+
+        for i_file in local_files.keys():
+            # Условие, при котором файл записывается в облачное хранилище:
+            if not remote_files or i_file not in remote_files.keys():
                 try:
-                    connector.update_file(i_file)
-                    logger_file.info('Файл {} успешно перезаписан'.format(i_file))
+                    connector.load_file(i_file)
+                    logger_file.info('Файл {} успешно записан'.format(i_file))
                 except (Timeout, TooManyRedirects, ConnectionError):
-                    logger_file.error('Файл {} не перезаписан. Ошибка соединения.'.format(i_file))
+                    logger_file.error('Файл {} не записан. Ошибка соединения.'.format(i_file))
                 except OSError:
                     logger_file.error("Ошибка чтения файла {}".format(i_file))
+            if remote_files:
+                # Условие, при котором файл обновляется в облачном хранилище:
+                if i_file in remote_files.keys() and local_files[i_file] > remote_files[i_file]:
+                    try:
+                        connector.update_file(i_file)
+                        logger_file.info('Файл {} успешно перезаписан'.format(i_file))
+                    except (Timeout, TooManyRedirects, ConnectionError):
+                        logger_file.error('Файл {} не перезаписан. Ошибка соединения.'.format(i_file))
+                    except OSError:
+                        logger_file.error("Ошибка чтения файла {}".format(i_file))
+    except (Timeout, TooManyRedirects, ConnectionError):
+        logger_file.error('Не удалось получить файлы с облачного сервиса. Ошибка соединения.')
 
 
 if __name__ == '__main__':
